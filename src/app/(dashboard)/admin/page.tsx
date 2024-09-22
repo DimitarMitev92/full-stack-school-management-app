77835c2d16fc07e0da6dc1d34954dade599d075a
Dimitar Mitev <d.mitev920123@gmail.com>
2024-09-22 22:19:15 +0300

    feat: Event calendar

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,7 +2,7 @@ import UserCard from "@/components/UserCard";
 import CountChart from "@/components/CountChart";
 import AttendanceChart from "@/components/AttendanceChart";
 import FinanceChart from "@/components/FinanceChart";
-
+import EventCalendar from "@/components/EventCalendar";
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
@@ -32,7 +32,9 @@ const AdminPage = () => {
         </div>
       </div>
       {/* RIGHT SIDE */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+      </div>
     </div>
   );
 };
