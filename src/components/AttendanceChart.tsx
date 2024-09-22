3b12d23dff5838eb3c5b69b2f351642819c5816a
Dimitar Mitev <d.mitev920123@gmail.com>
2024-09-22 20:19:54 +0300

    feat: count chart

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,13 @@
+const data = [
+  {
+    name: "Present",
+    count: 1234,
+    fill: "#FAE27C",
+  },
+];
+
+const AttendanceChart = () => {
+  return <div>AttendanceChart</div>;
+};
+
+export default AttendanceChart;
