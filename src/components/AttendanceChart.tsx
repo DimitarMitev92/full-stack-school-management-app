5e42bd60be0dfa5c62fe868aedff9fa0087d5b77
Dimitar Mitev <d.mitev920123@gmail.com>
2024-09-22 21:36:19 +0300

    feat: Finance chart

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -44,10 +44,12 @@ const data = [
 const AttendanceChart = () => {
   return (
     <div className="bg-white rounded-xl w-full h-full p-4">
+      {/* TITLE */}
       <div className="flex justify-between items-center">
         <h1 className="text-lg font-semibold">Attendance</h1>
         <Image src="/moreDark.png" alt="moreDark" width={20} height={20} />
       </div>
+      {/* CHART */}
       <ResponsiveContainer width="100%" height="90%">
         <BarChart width={500} height={300} data={data} barSize={20}>
           <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
