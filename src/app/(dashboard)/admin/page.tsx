625c1ce55028e8491a4db18a114dfd30caa3d177
Dimitar Mitev <d.mitev920123@gmail.com>
2024-09-23 09:53:12 +0300

    feat: Announcements

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -3,6 +3,7 @@ import CountChart from "@/components/CountChart";
 import AttendanceChart from "@/components/AttendanceChart";
 import FinanceChart from "@/components/FinanceChart";
 import EventCalendar from "@/components/EventCalendar";
+import Announcements from "@/components/Announcements";
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
@@ -34,6 +35,7 @@ const AdminPage = () => {
       {/* RIGHT SIDE */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
+        <Announcements />
       </div>
     </div>
   );
